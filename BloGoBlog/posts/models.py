f2064b0c23d02abc736201459a80ef30f9a92caf
Neha Nagpal <nehanagpal.nn34@gmail.com>
2018-03-18 22:41:19 +0530

    first commit

---
+++ BloGoBlog/posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+	title= models.CharField(max_length=120) 
+	content =models.TextField()
+	updated=models.DateTimeField(auto_now=True,auto_now_add=False)
+	timestamp=models.DateTimeField(auto_now=False,auto_now_add=True)
+	 
+	def _unicode_(self):
+	 	return self.title
+	def _str_(self):
+	 	return self.title
