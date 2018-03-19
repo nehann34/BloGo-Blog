bf49d16f6e108be03cf9b1fb5c8ff98e3aed312c
Neha Nagpal <nehanagpal.nn34@gmail.com>
2018-03-19 21:41:49 +0530

    Pagination donegit add .!

--- BloGoBlog/posts/models.py
+++ BloGoBlog/posts/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.core.urlresolvers import reverse
 # Create your models here.
 class Post(models.Model):
-	title= models.CharField(max_length=120) 
+	title= models.CharField(max_length=120)
+	image= models.FileField(null=True,blank=True)  
 	content =models.TextField()
 	updated=models.DateTimeField(auto_now=True,auto_now_add=False)
 	timestamp=models.DateTimeField(auto_now=False,auto_now_add=True)
@@ -14,3 +15,5 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return reverse("posts:retrieve",kwargs={"id":self.id})
 #		return "/posts/%s/" %(self.id)
+	def Meta():
+		ordering=["-id","-timestamp","-updated"]	
--- BloGoBlog/posts/views.py
+++ BloGoBlog/posts/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
 from .forms import PostForm
 from django.contrib import messages
 
 def post_create(request):
+	
 	form=PostForm(request.POST or None)
 	if form.is_valid():
 		instance=form.save(commit=False)
@@ -13,7 +17,7 @@ def post_create(request):
 		messages.success(request,"successfully created")
 	else:
 		messages.error(request,"Not successfully created")
-		return HttpResponseRedirect(instance.get_absolute_url())
+		#return HttpResponseRedirect(instance.get_absolute_url())
 
 	context={
 	"form":form,
@@ -27,13 +31,25 @@ def post_retrieve(request,id=None):
 	}
 	return render(request,"post_detail.html",context)
 def post_list(request):
-	queryset=Post.objects.all()
+	queryset_list=Post.objects.all()
+	paginator = Paginator(queryset_list, 25) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
 	context={
 		"object_list":queryset,
 		"title":"List"
 		}
 		
-	return render(request,"index.html",context)
+	return render(request,"post_list.html",context)
 
 
 def post_update(request,id=None):
@@ -52,5 +68,9 @@ def post_update(request,id=None):
 	}
 	return render(request,"post_form.html",context)
 
-def post_delete(request):
-	return HttpResponse("<h1>Delete</h1>")
+def post_delete(request,id=None):
+	instance=get_object_or_404(Post,id=id)
+	instance.delete()
+	messages.success(request,"successfully deleted")
+	return redirect("posts:list")
+	
